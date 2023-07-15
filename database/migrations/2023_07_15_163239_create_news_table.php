e9e2e5b8d8120ad4e771a31f36cd592502db03fb
yasin <yascanary0@gmail.com>
2023-07-15 20:12:06 +0330

    Create news files and schema

---
+++ database/migrations/2023_07_15_163239_create_news_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id()->index();
+            $table->string("title");
+            $table->longText("content");
+            $table->string("thumbnail");
+            $table->bigInteger("likes_count")->default(0);
+            $table->bigInteger("dislikes_count")->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
